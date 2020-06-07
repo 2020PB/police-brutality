314330cdcde898fb474e4757e67142d37f25dcdd
postables <postables@rtradetechnologies.com>
2020-06-07 01:34:26 -0700

    tools/downloader: add basic screenshotter

--- tools/downloader/go.mod
+++ tools/downloader/go.mod
@@ -4,10 +4,11 @@ go 1.14
 
 require (
 	github.com/RTradeLtd/go-ipfs-api/v3 v3.0.0
+	github.com/chromedp/cdproto v0.0.0-20200116234248-4da64dd111ac
+	github.com/chromedp/chromedp v0.5.3
 	github.com/panjf2000/ants/v2 v2.4.1
 	github.com/pkg/errors v0.8.1
 	github.com/urfave/cli/v2 v2.2.0
 	go.bobheadxi.dev/zapx/zapx v0.6.8
-	go.uber.org/atomic v1.6.0
 	go.uber.org/zap v1.15.0
 )
---
+++ tools/downloader/pkg/screenshotter.go
@@ -0,0 +1,97 @@
+// Command screenshot is a chromedp example demonstrating how to take a
+// screenshot of a specific element and of the entire browser viewport.
+
+package pkg
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"math"
+
+	"github.com/chromedp/cdproto/emulation"
+	"github.com/chromedp/cdproto/page"
+	"github.com/chromedp/chromedp"
+)
+
+/*
+copied from and modified from https://github.com/chromedp/examples/blob/master/screenshot/main.go
+*/
+
+func capture(url string, name string) {
+	// create context
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	// capture screenshot of an element
+	var buf []byte
+	if err := chromedp.Run(ctx, elementScreenshot(url, `#main`, &buf)); err != nil {
+		log.Fatal(err)
+	}
+	if err := ioutil.WriteFile(name+"-elementScreenshot.png", buf, 0644); err != nil {
+		log.Fatal(err)
+	}
+
+	// capture entire browser viewport, returning png with quality=90
+	if err := chromedp.Run(ctx, fullScreenshot(url, 10, &buf)); err != nil {
+		log.Fatal(err)
+	}
+	if err := ioutil.WriteFile(name+"-fullScreenshot.png", buf, 0644); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// elementScreenshot takes a screenshot of a specific element.
+func elementScreenshot(urlstr, sel string, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(urlstr),
+		chromedp.WaitVisible(sel, chromedp.ByID),
+		chromedp.Screenshot(sel, res, chromedp.NodeVisible, chromedp.ByID),
+	}
+}
+
+// fullScreenshot takes a screenshot of the entire browser viewport.
+//
+// Liberally copied from puppeteer's source.
+//
+// Note: this will override the viewport emulation settings.
+func fullScreenshot(urlstr string, quality int64, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(urlstr),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			// get layout metrics
+			_, _, contentSize, err := page.GetLayoutMetrics().Do(ctx)
+			if err != nil {
+				return err
+			}
+
+			width, height := int64(math.Ceil(contentSize.Width)), int64(math.Ceil(contentSize.Height))
+
+			// force viewport emulation
+			err = emulation.SetDeviceMetricsOverride(width, height, 1, false).
+				WithScreenOrientation(&emulation.ScreenOrientation{
+					Type:  emulation.OrientationTypePortraitPrimary,
+					Angle: 0,
+				}).
+				Do(ctx)
+			if err != nil {
+				return err
+			}
+
+			// capture screenshot
+			*res, err = page.CaptureScreenshot().
+				WithQuality(quality).
+				WithClip(&page.Viewport{
+					X:      contentSize.X,
+					Y:      contentSize.Y,
+					Width:  contentSize.Width,
+					Height: contentSize.Height,
+					Scale:  1,
+				}).Do(ctx)
+			if err != nil {
+				return err
+			}
+			return nil
+		}),
+	}
+}
